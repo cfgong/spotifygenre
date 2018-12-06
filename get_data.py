260814a104c9afe1cc617c9db5f418c9d8a3dfe2
Crystal Gong <crystalgong123@gmail.com>
2018-12-06 13:48:04 -0600

    working on get playlist content

--- get_data.py
+++ get_data.py
@@ -1,7 +1,6 @@
-import json
 import spotipy
-import pandas as pd
 
+import pandas as pd
 from pandas.io.json import json_normalize # for flattening json files
 
 import credentials as creds #credentials file rip
@@ -11,38 +10,60 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # client_credentials_manager = SpotifyClientCredentials(client_id='Your_Client_ID', client_secret='Your_Client_Secret')
 client_credentials_manager = SpotifyClientCredentials(client_id=creds.CLIENT_ID, client_secret=creds.CLIENT_SECRET)
 
+"""
+Takes in a username, and playlist_id
+Get all the playlists of a user.
+Return a pandas df with the playlists's ids, names, and numbers of tracks. 
+"""
 
+def get_playlist_content(username, playlist_id, sp):
+    content = sp.user_playlist_tracks(username, playlist_id, fields=None, limit=100, offset=0, market=None)
+    content = sp.user_playlists(username)
+    content_df = json_normalize(content['items'])
+    print(content)
+    print(content_df)
+    return content
 
+"""
+implement this
+"""
+def get_playlist_audio_features(username, playlist_id, sp):
+    return None
+
+"""
+Takes in a username. 
+Get all the playlists of a user.
+Return a pandas df with the playlists's ids, names, and numbers of tracks. 
+"""
 
 def get_user_playlists(username, sp):
     playlists = sp.user_playlists(username)
     playlist_df = json_normalize(playlists['items'])
     
-    # Print original attributes before removing unncessary ones
-    """
-    attribute_names = playlist_df.columns.values.tolist()
-    print("Original attributes: ", attribute_names, "\n")
-    """
-    
     features_to_remove = ['collaborative', 'external_urls.spotify', 'href', 'images', 'owner.display_name', 'owner.external_urls.spotify', 'owner.href', 'owner.id', 'owner.type', 'owner.uri', 'primary_color', 'public', 'snapshot_id', 'tracks.href', 'type', 'uri'] 
-    # print("Features to remove: ", features_to_remove, "\n")
     playlist_df = playlist_df.drop(features_to_remove, axis = 1)
     attribute_names = playlist_df.columns.values.tolist()
+    
     print("Feature attributes: ", attribute_names, "\n")
     print(playlist_df)
+    
     return playlist_df
 
 def main(username, *playlist):
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     print ("Getting user playlist")
     playlist_df = get_user_playlists(username, sp)
-    """
+    
+    playlist_id = playlist_df["id"][0]
+
     print ("Getting playlist content")
-    get_playlist_content(username, playlist, sp)
+    content_df = get_playlist_content(username, playlist_id, sp)
+    
+    """
     print ("Getting playlist audio features")
     get_playlist_audio_features(username, playlist, sp)
     """
-    return playlist_df
+    return playlist_df, content_df
 
 if __name__ == '__main__':
-    playlist_df = main(creds.USERNAME)
+    playlist_df, content_df = main(creds.USERNAME)
