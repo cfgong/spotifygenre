dc412625673b21cbdd098a075537d7dac0802c34
Crystal Gong <crystalgong123@gmail.com>
2018-12-07 01:31:18 -0600

    add functionality to write user song tracks list to a file

--- get_data.py
+++ get_data.py
@@ -1,5 +1,4 @@
 import spotipy
-import json 
 import pandas as pd
 from pandas.io.json import json_normalize # for flattening json files
 
@@ -8,7 +7,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 # shhh secret stuff, credentials in crentials.py
 # client_credentials_manager = SpotifyClientCredentials(client_id='Your_Client_ID', client_secret='Your_Client_Secret')
-client_credentials_manager = SpotifyClientCredentials(client_id=creds.CLIENT_ID, client_secret=creds.CLIENT_SECRET)
+# client_credentials_manager = SpotifyClientCredentials(client_id=creds.CLIENT_ID, client_secret=creds.CLIENT_SECRET)
 
 """
 Takes in a username. 
@@ -33,36 +32,53 @@ def get_user_playlists(username, sp):
 """
 takes in a playlist json and then prints the playlist name, total tracks, and track info
 """
-def print_playlist_tracks(username, playlist):
-    # print('%s: PLAYLIST: %s' % (playlist['id'], playlist['name']))
-    print('PLAYLIST: %s' % (playlist['name']))
-    print('TOTAL TRACKS: %d' % (playlist['tracks']['total']))
+def print_playlist_tracks(username, playlist, sp, filename = None):
+    playlistName = playlist['name']
+    trackCount = playlist['tracks']['total']
+    playlistID = playlist['id']
     
-    results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+    if filename is not None:
+        with open(filename, "a", encoding="utf-8") as text_file:
+            print(f'PLAYLIST: {playlistName}', file = text_file)
+            print(f'TOTAL TRACKS: {trackCount}', file = text_file)
+    else:
+        # print(f'PLAYLIST: {playlistID} : {playlistName}')
+        print(f'PLAYLIST: {playlistName}')
+        print(f'TOTAL TRACKS: {trackCount}')
+
+    results = sp.user_playlist(username, playlistID, fields="tracks,next")
     tracks = results['tracks']
     # print(tracks)
-    print_tracks(tracks)
+    print_tracks(tracks, filename)
     while tracks['next']:
         tracks = sp.next(tracks)
-        print_tracks(tracks)
+        print_tracks(tracks, filename)
 
 """
 take in tracks json and prints tracks
 """
-def print_tracks(tracks):
+def print_tracks(tracks, filename):
     for i, item in enumerate(tracks['items']):
         # i is the track index, item gives us the track info
         track = item['track']
+        
+        artistName = track['artists'][0]['name']
+        trackName = track['name']
+        trackID = track['id']
         # print artist name and item 
-        # print("- %s: %s - %s" % (track['id'], track['artists'][0]['name'], track['name']))
-        print("- %s - %s" % (track['artists'][0]['name'], track['name']))
+        if filename is not None:
+            with open(filename, "a", encoding="utf-8") as text_file:
+                print(f"- {artistName} - {trackName}", file = text_file)
+        else:
+            # print(f"- {trackID} : {artistName} - {trackName}")
+            print(f"- {artistName} - {trackName}")
 """
 print all tracks of a user
 """
-def print_all_user_tracks(username, sp):
-    playlists = sp.user_playlists(creds.USERNAME)
+def print_all_user_tracks(username, sp, filename = None):
+    playlists = sp.user_playlists(username)
     for playlist in playlists['items']:
-        print_playlist_tracks(creds.USERNAME, playlist)
+        print_playlist_tracks(username, playlist, sp, filename)
 """
 gets all playlists of a user, gets all the tracks, and returns a df of all tracks
 """
@@ -70,7 +86,7 @@ def get_playlist_content(username, sp):
     columns = ['playlist.id', 'playlist.name', 'id', 'artist', 'title']
     tracks_df = pd.DataFrame(columns= columns) #dataframe to hold the track data
     
-    playlists = sp.user_playlists(creds.USERNAME)
+    playlists = sp.user_playlists(username)
     for playlist in playlists['items']:
         playlistName = playlist['name']
         playlistID = playlist['id']
@@ -101,19 +117,48 @@ def get_playlist_content(username, sp):
     return tracks_df
 
 """
-implement this
+gets all playlists of a user, gets all the tracks, and returns a df of track features
 """
 def get_playlist_features(username, sp):
     return None
-   
-if __name__ == '__main__':
+"""
+get playlist_df, tracks_df, features_df using usercredentials
+"""
+def main(username, client_ID, client_secret):
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_ID, client_secret=client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    print ("Getting user's playlists details")
-    playlist_df = get_user_playlists(creds.USERNAME, sp)
     
-    print("Getting all tracks from user's playlists")
-    tracks_df = get_playlist_content(creds.USERNAME, sp)
-    # print_all_user_tracks(creds.USERNAME, sp)
-    print("Getting features from all tracks in the user's playlists")
-    features_df = get_playlist_features(creds.USERNAME, sp)
+    playlist_df = get_user_playlists(username, sp)
+    # print ("User's playlists details: playlist_df")
+    tracks_df = get_playlist_content(username, sp)
+    # print("All tracks from user's playlists: tracks_df")
+    features_df = get_playlist_features(username, sp)
+    # print("All audio features of tracks in the user's playlists: features_df")
+    return playlist_df, tracks_df, features_df
+
+"""
+print all user tracks
+you can pass an optional argument with filename
+"""
+def main2(username, client_ID, client_secret, filename = None):
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_ID, client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    
+    # clear file first
+    if filename is not None:
+        open(filename, 'w').close()
+        
+    print_all_user_tracks(username, sp, filename)
+
+if __name__ == '__main__':
+    username = creds.USERNAME
+
+    playlist_df, tracks_df, features_df = main(username, creds.CLIENT_ID, creds.CLIENT_SECRET)
+    
+    # write user song tracks list to a file
+    """
+    filename = username + "output.txt"
+    main2(username, creds.CLIENT_ID, creds.CLIENT_SECRET, filename)
+    """    
+
     
