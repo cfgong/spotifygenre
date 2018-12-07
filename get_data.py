a490477368fcfce85697c177411ea960b3fbb94c
Crystal Gong <crystalgong123@gmail.com>
2018-12-07 00:16:16 -0600

    implemented function to get all user track data and input into a pandas df

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 import spotipy
-
+import json 
 import pandas as pd
 from pandas.io.json import json_normalize # for flattening json files
 
@@ -11,26 +11,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_credentials_manager = SpotifyClientCredentials(client_id=creds.CLIENT_ID, client_secret=creds.CLIENT_SECRET)
 
 """
-Takes in a username, and playlist_id
-Get all the playlists of a user.
-Return a pandas df with the playlists's ids, names, and numbers of tracks. 
-"""
-
-def get_playlist_content(username, playlist_id, sp):
-    content = sp.user_playlist_tracks(username, playlist_id, fields=None, limit=100, offset=0, market=None)
-    content = sp.user_playlists(username)
-    content_df = json_normalize(content['items'])
-    print(content)
-    print(content_df)
-    return content
-
-"""
-implement this
-"""
-def get_playlist_audio_features(username, playlist_id, sp):
-    return None
-
-"""
 Takes in a username. 
 Get all the playlists of a user.
 Return a pandas df with the playlists's ids, names, and numbers of tracks. 
@@ -39,31 +19,101 @@ Return a pandas df with the playlists's ids, names, and numbers of tracks.
 def get_user_playlists(username, sp):
     playlists = sp.user_playlists(username)
     playlist_df = json_normalize(playlists['items'])
-    
+ 
     features_to_remove = ['collaborative', 'external_urls.spotify', 'href', 'images', 'owner.display_name', 'owner.external_urls.spotify', 'owner.href', 'owner.id', 'owner.type', 'owner.uri', 'primary_color', 'public', 'snapshot_id', 'tracks.href', 'type', 'uri'] 
     playlist_df = playlist_df.drop(features_to_remove, axis = 1)
+    
+    # print attribute names
+    """
     attribute_names = playlist_df.columns.values.tolist()
-    
     print("Feature attributes: ", attribute_names, "\n")
-    print(playlist_df)
-    
+    """
     return playlist_df
 
-def main(username, *playlist):
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    print ("Getting user playlist")
-    playlist_df = get_user_playlists(username, sp)
+"""
+takes in a playlist json and then prints the playlist name, total tracks, and track info
+"""
+def print_playlist_tracks(username, playlist):
+    # print('%s: PLAYLIST: %s' % (playlist['id'], playlist['name']))
+    print('PLAYLIST: %s' % (playlist['name']))
+    print('TOTAL TRACKS: %d' % (playlist['tracks']['total']))
     
-    playlist_id = playlist_df["id"][0]
+    results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+    tracks = results['tracks']
+    # print(tracks)
+    print_tracks(tracks)
+    while tracks['next']:
+        tracks = sp.next(tracks)
+        print_tracks(tracks)
 
-    print ("Getting playlist content")
-    content_df = get_playlist_content(username, playlist_id, sp)
+"""
+take in tracks json and prints tracks
+"""
+def print_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        # i is the track index, item gives us the track info
+        track = item['track']
+        # print artist name and item 
+        # print("- %s: %s - %s" % (track['id'], track['artists'][0]['name'], track['name']))
+        print("- %s - %s" % (track['artists'][0]['name'], track['name']))
+"""
+print all tracks of a user
+"""
+def print_all_user_tracks(username, sp):
+    playlists = sp.user_playlists(creds.USERNAME)
+    for playlist in playlists['items']:
+        print_playlist_tracks(creds.USERNAME, playlist)
+"""
+gets all playlists of a user, gets all the tracks, and returns a df of all tracks
+"""
+def get_playlist_content(username, sp):
+    columns = ['playlist.id', 'playlist.name', 'id', 'artist', 'title']
+    tracks_df = pd.DataFrame(columns= columns) #dataframe to hold the track data
     
-    """
-    print ("Getting playlist audio features")
-    get_playlist_audio_features(username, playlist, sp)
-    """
-    return playlist_df, content_df
+    playlists = sp.user_playlists(creds.USERNAME)
+    for playlist in playlists['items']:
+        playlistName = playlist['name']
+        playlistID = playlist['id']
+        
+        results = sp.user_playlist(username, playlistID, fields="tracks,next")
+        tracks = results['tracks']
+        
+        """
+        appends track data of each track into the tracks_df dataframe
+        """
+        def input_track_data(tracks, tracks_df):
+            for i, item in enumerate(tracks['items']):
+                track = item['track']
+                
+                artistName = track['artists'][0]['name']
+                trackName = track['name']
+                trackID = track['id']
+                
+                df = pd.DataFrame([[playlistID, playlistName, trackID, artistName, trackName]], columns = columns)
+                tracks_df = tracks_df.append(df, ignore_index = True)
+            return tracks_df
+        
+        tracks_df = input_track_data(tracks, tracks_df)
+        
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            tracks_df = input_track_data(tracks, tracks_df)
+    return tracks_df
 
+"""
+implement this
+"""
+def get_playlist_features(username, sp):
+    return None
+   
 if __name__ == '__main__':
-    playlist_df, content_df = main(creds.USERNAME)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    print ("Getting user's playlists details")
+    playlist_df = get_user_playlists(creds.USERNAME, sp)
+    
+    print("Getting all tracks from user's playlists")
+    tracks_df = get_playlist_content(creds.USERNAME, sp)
+    # print_all_user_tracks(creds.USERNAME, sp)
+    print("Getting features from all tracks in the user's playlists")
+    features_df = get_playlist_features(creds.USERNAME, sp)
+    
